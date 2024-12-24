80a5c5a325546ffb3143e7b8b55e82adb0b22046
Eyara Jadyn Reyes <eyarareyes13@gmail.com>
2024-12-24 16:20:48 +0800

    design test

--- StudentScholarshipProgram/Form1.Designer.vb
+++ StudentScholarshipProgram/Form1.Designer.vb
@@ -24,11 +24,12 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Button1 = New Button()
         Button2 = New Button()
+        Button3 = New Button()
         SuspendLayout()
         ' 
         ' Button1
         ' 
-        Button1.Location = New Point(0, 0)
+        Button1.Location = New Point(55, 367)
         Button1.Name = "Button1"
         Button1.Size = New Size(75, 23)
         Button1.TabIndex = 0
@@ -37,18 +38,28 @@ Partial Class Form1
         ' 
         ' Button2
         ' 
-        Button2.Location = New Point(400, 302)
+        Button2.Location = New Point(492, 330)
         Button2.Name = "Button2"
-        Button2.Size = New Size(388, 120)
+        Button2.Size = New Size(277, 96)
         Button2.TabIndex = 1
         Button2.Text = "Button2"
         Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(259, 367)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(75, 23)
+        Button3.TabIndex = 2
+        Button3.Text = "Button3"
+        Button3.UseVisualStyleBackColor = True
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(Button3)
         Controls.Add(Button2)
         Controls.Add(Button1)
         Name = "Form1"
@@ -58,5 +69,6 @@ Partial Class Form1
 
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
 
 End Class
